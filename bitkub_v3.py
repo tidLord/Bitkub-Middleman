1d6df903152d79532302e8963aa35ac418de25f1
tar <tar.lovestudy@gmail.com>
2024-02-10 07:14:03 +0700

    Hello World

---
+++ bitkub_v3.py
@@ -0,0 +1,38 @@
+import json, hmac, requests, hashlib, time
+
+def bitkub(reqType, reqPath, reqBody, reqCredentials):
+    def gen_sign(api_secret, payload_string=None):
+        return hmac.new(api_secret.encode('utf-8'), payload_string.encode('utf-8'), hashlib.sha256).hexdigest()
+    ts = str(round(time.time() * 1000))
+    host = 'https://api.bitkub.com'
+    url = host + reqPath
+    payload = []
+    payload.append(ts)
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-BTK-TIMESTAMP': ts,
+        'X-BTK-APIKEY': reqCredentials['apiKey']
+    }
+    if reqType == "GET":
+        def gen_query_param(url, query_param):
+            req = requests.PreparedRequest()
+            req.prepare_url(url, query_param)
+            return req.url.replace(url,"")
+        query_param = gen_query_param(host+reqPath, reqBody)
+        payload.append('GET')
+        payload.append(reqPath)
+        payload.append(query_param)
+        sig = gen_sign(reqCredentials['secretKey'], ''.join(payload))
+        headers['X-BTK-SIGN'] = sig
+        response = requests.request('GET', f'{host}{reqPath}{query_param}', headers=headers, data={}, verify=True)
+        return response.text
+    if reqType == "POST":
+        payload.append('POST')
+        payload.append(reqPath)
+        payload.append(json.dumps(reqBody))
+        sig = gen_sign(reqCredentials['secretKey'], ''.join(payload))
+        headers['X-BTK-SIGN'] = sig
+        response = requests.request('POST', url, headers=headers, data=json.dumps(reqBody), verify=True)
+        return response.text
+    
---
+++ example.py
@@ -0,0 +1,48 @@
+from bitkub_v3 import bitkub
+
+### สำหรับ Non-secure endpoints
+credentials = {
+    'apiKey': '',
+    'secretKey': ''
+}
+
+# GET แบบที่ไม่ต้องการ Body
+reqBody = {
+}      
+print(bitkub("GET", '/api/status', reqBody, credentials))
+
+# GET แบบที่ต้องการ Body
+reqBody = {
+    'symbol': 'BTC_THB',
+    'resolution': '1',
+    'from': 1633424427,
+    'to' : 1633427427
+}      
+print(bitkub("GET", '/tradingview/history', reqBody, credentials))
+
+
+### สำหรับ Secure endpoints ###
+credentials = {
+    'apiKey': 'xxx',
+    'secretKey': 'xxx'
+}
+
+# GET แบบ Secure endpoints ที่ต้องการ Body
+reqBody = {
+    'hash': 'fwQ6dnQYKnqFPYSn2u25cNDfRsN'
+}      
+print(bitkub("GET", '/api/v3/market/order-info', reqBody, credentials))
+
+# POST แบบที่ไม่ต้องการ Body
+reqBody = {
+}      
+print(bitkub("POST", '/api/v3/user/trading-credits', reqBody, credentials))
+
+# POST แบบที่ต้องการ Body
+reqBody = {
+    'sym': 'btc_thb',
+    'amt': 999,
+    'rat': 999999,
+    'typ': 'limit'
+}      
+print(bitkub("POST", '/api/v3/market/place-bid', reqBody, credentials))
